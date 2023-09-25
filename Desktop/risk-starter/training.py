4cce1716d01fcbae7aecdf1a448337ee776c8890
BorchaniSalma <sborcheni@ieee.org>
2023-09-24 21:08:12 -0600

    training logistic Regression model

--- Desktop/risk-starter/training.py
+++ Desktop/risk-starter/training.py
@@ -1,32 +1,43 @@
-from flask import Flask, session, jsonify, request
+# from flask import Flask, session, jsonify, request
 import pandas as pd
-import numpy as np
+# import numpy as np
 import pickle
 import os
-from sklearn import metrics
-from sklearn.model_selection import train_test_split
+# from sklearn import metrics
+# from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 import json
 
-###################Load config.json and get path variables
-with open('config.json','r') as f:
-    config = json.load(f) 
+# Load config.json and get path variables
+with open('config.json', 'r') as f:
+    config = json.load(f)
 
-dataset_csv_path = os.path.join(config['output_folder_path']) 
-model_path = os.path.join(config['output_model_path']) 
+dataset_csv_path = os.path.join(config['output_folder_path'])
+model_path = os.path.join(config['output_model_path'])
 
 
-#################Function for training the model
+# Function for training the model
 def train_model():
-    
-    #use this logistic regression for training
-    LogisticRegression(C=1.0, class_weight=None, dual=False, fit_intercept=True,
-                    intercept_scaling=1, l1_ratio=None, max_iter=100,
-                    multi_class='warn', n_jobs=None, penalty='l2',
-                    random_state=0, solver='liblinear', tol=0.0001, verbose=0,
-                    warm_start=False)
-    
-    #fit the logistic regression to your data
-    
-    #write the trained model to your workspace in a file called trainedmodel.pkl
 
+    # use this logistic regression for training
+    logit = LogisticRegression(C=1.0, class_weight=None, dual=False,
+                               fit_intercept=True,
+                               intercept_scaling=1, l1_ratio=None,
+                               max_iter=100,
+                               multi_class='auto', n_jobs=None, penalty='l2',
+                               random_state=0, solver='liblinear', tol=0.0001,
+                               verbose=0,
+                               warm_start=False)
+
+    # fit the logistic regression to your data
+    trainingdata = pd.read_csv(dataset_csv_path+'/'+'finaldata.csv')
+    X = trainingdata[['lastmonth_activity', 'lastyear_activity',
+                      'number_of_employees']]
+    y = trainingdata['exited']
+    model = logit.fit(X, y)
+    # Save model
+    pickle.dump(model, open(model_path + '/' 'trainedmodel.pkl', 'wb'))
+
+
+if __name__ == '__main__':
+    train_model()
